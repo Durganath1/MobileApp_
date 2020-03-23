25766dca5d67983f2c67437bdb7bc2bf295a79b1
Durganath1 <durganath@a1expresscarremoval.com.au>
2020-03-23 19:26:52 +0800

    Commit

---
+++ a1ExpressVol_1/a1ExpressVol_1/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace a1ExpressVol_1
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Product> Products { get; set; }
+
+        public MainViewModel()
+        {
+            Products = new ObservableCollection<Product>
+            {
+                new Product
+                {
+                    Title = "Job No 1",
+                    IsVisible = false
+                },
+                new Product
+                {
+                    Title = "Job No 2",
+                },
+                new Product
+                {
+                    Title = "Job No 3"
+                }
+            };
+        }
+        public void HideOrShowProduct(Product product)
+        {
+            product.IsVisible = true;
+            UpdateProducts(product);
+        }
+
+        private void UpdateProducts(Product product)
+        {
+            var index = Products.IndexOf(product);
+            Products.Remove(product);
+            Products.Insert(index, product);
+        }
+    }
+}
---
+++ a1ExpressVol_1/a1ExpressVol_1/View/DashboardPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace a1ExpressVol_1.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DashboardPage : ContentPage
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var vm = BindingContext as MainViewModel;
+            var product = e.Item as Product;
+            
+            
+        }
+    }
+    
+}
